34e992444bc423006303fb27db0540044aeba4c1
Ruqayah22 <ruqayah.alnawfaly@outlook.com>
2021-09-19 17:56:59 +0300

    add files

---
+++ week-2/Q8.js
@@ -0,0 +1,7 @@
+// Q8 Write a program to validate if the number is from multiples of 2 .
+// ex: console.log(isMultiplesOfTwo(2))     output: true
+// ex: console.log(isMultiplesOfTwo(512))     output: true
+// ex: console.log(isMultiplesOfTwo(513))     output: false
+// ex: console.log(isMultiplesOfTwo(255))     output: false
+
+function isMultiplesOfTwo(num)
